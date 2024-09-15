705d09e416ae9934a9ba389e68f0a900a1dbdc2d
Vansh Dixit <officialvanshdixit@gmail.com>
2024-09-15 07:47:38 +0530

    add 02datatypes.js

--- 02datatypes.js
+++ 02datatypes.js
@@ -2,3 +2,25 @@
 
 // alert("hello there.. ") .......... we are using node js and not browser. 
 // it works inside console on a browser or when integrated with the html
+
+
+let name = "vansh is the first sextillionaire" // string
+let age = 160 // integer
+let isSexy = true // boolean
+
+/*
+number --> range is 2^53
+bigint
+boolean
+string
+null --> standalone value and kind of a datatype
+undefined --> value is not assigned
+symbol --> for uniqueness 
+
+object 
+
+*/
+
+console.log(typeof  "spexy vansh")
+console.log(typeof null) // the output says that null is an object`
+console.log (typeof undefined ) // undefined 
