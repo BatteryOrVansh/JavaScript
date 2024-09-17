e2db03e41a407f9b5338e4ade4d61df54d753ed7
Vansh Dixit <officialvanshdixit@gmail.com>
2024-09-17 16:26:53 +0530

    add 06stackAndHeapMemory

---
+++ 06stackAndHeapMemory.js
@@ -0,0 +1,17 @@
+/*
+
+datatypes are based on memory allocation
+Primitive datatypes->  call by value (string,number,boolean,null,undefined,BigInt,symbol) ->Stack memory
+
+Reference/Non primitive data types -> call by reference ( array,object, function) -> heap memory
+
+
+best example : https://www.youtube.com/watch?v=7gwc-1czolw&list=PLu71SKxNbfoBuX3f4EOACle2y-tRC5Q37&index=10
+
+
+
+
+from GfG: 
+Stack: It is a data structure used to store static data. Static data refers to data whose size is known by the engine during compile time. In JavaScript, static data includes primitive values like strings, numbers, boolean, null, and undefined. References that point to objects and functions are also included. A fixed amount of memory is allocated for static data. This process is known as static memory allocation.
+Heap: It is used to store objects and functions in JavaScript. The engine doesn’t allocate a fixed amount of memory. Instead, it allocates more space as required.
+*/
